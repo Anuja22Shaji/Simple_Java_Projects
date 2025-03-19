28925ed4d8d91e39bf58213096c95dc68fc78c79
Anuja <anujashaji@gmail.com>
2025-03-19 20:25:40 +0000

    to understand oops concept

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,44 @@
 public class BankAccount {
+    private String acctHolder;
+    private double balance;
+    public BankAccount(String aactHolder ,double initialbalance)
+    {
+        this.acctHolder=aactHolder;
+        this.balance=initialbalance;
+
+    }
+    public void deposite(double amount){
+        if(amount>0){
+            balance += amount;
+            System.out.println("deposited "+ amount);
+        }
+        else{
+            System.out.println("Deposite must be positive");
+        }
+    }
+    public void withdraw(double amount){
+        if(balance>0&& amount<=balance){
+            balance -= amount;
+            System.out.println("amount withdraw"+amount);
+        }
+        else{
+            System.out.println("invalid amount");
+        }
+    }
+    public void checkbalance()
+    {
+        System.out.println("balance is"+ balance);
+
+    }
     
+
+public static void main(String[] args) {
+    BankAccount acct= new BankAccount("Mary Angel",1000);
+    acct.checkbalance();
+    acct.deposite(600);
+    acct.checkbalance();
+    acct.withdraw(500);
+    acct.checkbalance();
 }
+}
+
